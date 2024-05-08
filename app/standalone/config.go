72fd607cbf9bc7c881e85b73d1a18d2bfa504614
Andreas Pfurtscheller <1051396+aplr@users.noreply.github.com>
2024-05-09 01:09:31 +0200

    squash

--- app/standalone/config.go
+++ app/standalone/config.go
@@ -4,5 +4,5 @@ import "github.com/lambda-feedback/shimmy/internal/server"
 
 type Config struct {
 	// HttpConfig represents the configuration for the HTTP server.
-	HttpConfig server.HttpConfig `conf:"http,squash"`
+	HttpConfig server.HttpConfig `conf:",squash"`
 }
